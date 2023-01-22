4025bbc5c51c4fbef6b0c19cb302a83eccf2610a
DevBaddy <warbaddy19+github@gmail.com>
2023-01-22 16:59:58 -0400

    Added Week 007 solution

---
+++ week-007.js
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/contains-duplicate/
+
+/* Given an integer array nums, return true if any value appears at least twice in the array, and return 
+false if every element is distinct. */
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+
+var containsDuplicate = function (nums) {
+  let numSet = new Set(nums);
+
+  return numSet.size !== nums.length;
+};
