91330512b460bef8e4eea5b1e3ffa8dddb7f29f5
DevBaddy <warbaddy19+github@gmail.com>
2023-03-29 21:11:01 -0300

    Updated Week 017 solution

--- week-017.py
+++ week-017.py
@@ -1,41 +1,22 @@
 from math import floor
 
-class Solution:
-    def convertToBase7(self, num: int) -> str:  
-        base7 = []
+def convertnumbertobase7(num):
+    if num == 0:
+        return "0"
 
-        if num == 0:
-            return "0"
+    base7 = []
+    quotient = abs(num)
 
-        if num < 7 and num > 0:
-            return str(num)  
+    while quotient > 0:
+        remainder = floor(quotient % 7)
 
-        if num < 0 and num > -7:
-            return str(num)      
+        base7.append(remainder)
+        quotient = quotient // 7
 
-        if num > 0:
-            while round(num) > 0:
-                base7.append(floor(num % 7))
-                num = num / 7
+    base7.reverse()
+    base7 = ''.join(str(x) for x in base7)
 
-            base7.reverse()
-            base7 = ''.join(str(x) for x in base7)
+    return base7 if num > 0 else "-" + base7
 
-            if base7[0] == "0":
-                base7 = base7[1:]
-        else:
-            num = abs(num)
 
-            while round(num) > 0:
-                base7.append(floor(num % 7))
-                num = num / 7
-
-            base7.reverse()
-            base7 = ''.join(str(x) for x in base7)
-
-            if base7[0] == "0":
-                base7 = base7[1:]
-
-            base7 = "-" + base7
-
-        return base7
+print(convertnumbertobase7(4))
