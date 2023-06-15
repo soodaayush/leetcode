09d026b41510cc6d64c2f912f91f205936b5bd10
DevBaddy <warbaddy19+github@gmail.com>
2023-06-15 17:50:34 -0300

    Added Week 028 solution

--- week-027.py
+++ week-027.py
@@ -1,3 +1,11 @@
+# https://leetcode.com/problems/single-number-ii/
+
+# Given an integer array nums where every element appears three times except for one,
+# which appears exactly once. Find the single element and return it.
+
+# You must implement a solution with a linear runtime complexity and use only constant extra
+# space.
+
 def singleNumber(nums: list[int]):
     for i in nums:
         if nums.count(i) == 1:
