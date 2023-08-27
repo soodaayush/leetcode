6d02993e3e518ead838afaf468d5422b71077bc8
DevBaddy <warbaddy19+github@gmail.com>
2023-08-27 11:34:33 -0300

    Added Week 039 solution

--- week-038.py
+++ week-038.py
@@ -1,4 +1,4 @@
-# https://leetcode.com/problems/move-zeroes/description/
+# https://leetcode.com/problems/move-zeroes/
 
 # Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero
 # elements.
