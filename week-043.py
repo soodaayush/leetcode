5816bba076c29aed1d8c09f9d9611e79023d4293
DevBaddy <warbaddy19+github@gmail.com>
2023-10-08 10:53:14 -0300

    Added Week 043 solution

---
+++ week-043.py
@@ -0,0 +1,22 @@
+# https://leetcode.com/problems/majority-element/
+
+# Given an array nums of size n, return the majority element.
+
+# The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the
+# majority element always exists in the array.
+
+def majorityElement(nums: list[int]) -> int:
+    numbersAmount = {}
+
+    for i in nums:
+        if i in numbersAmount.keys():
+            continue
+
+        numbersAmount[i] = nums.count(i)
+
+    maxNum = max(numbersAmount, key=numbersAmount.get)
+
+    return maxNum
+
+
+print(majorityElement([2,2,1,1,1,2,2]))
