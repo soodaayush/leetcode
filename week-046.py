5f701fe1f8b1ef60830724891d70adcd24497f7e
DevBaddy <warbaddy19+github@gmail.com>
2024-02-11 16:51:09 -0400

    Added Week 046 solution

--- week-046.py
+++ week-046.py
@@ -1,5 +1,6 @@
 def mostCommonWord(paragraph: str, banned: list[str]) -> str:
     paragraph = paragraph.lower()
+    wordCount = {}
 
     symbols = "!?',;."
 
@@ -7,7 +8,17 @@ def mostCommonWord(paragraph: str, banned: list[str]) -> str:
         paragraph = paragraph.replace(i, " ")
     paragraph = paragraph.split()
 
-    print(paragraph)
+    for i in paragraph:
+        if i not in wordCount:
+            wordCount[i] = 0
+        if i in wordCount:
+            wordCount[i] += 1
+
+    for i in banned:
+        if i in wordCount:
+            del wordCount[i]
+
+    return max(wordCount, key=wordCount.get)
 
 
 print(mostCommonWord("Bob hit a ball, the hit BALL flew far after it was hit.", ["hit"]))
