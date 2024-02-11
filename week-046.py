ea699f9f564acfa0a923a0e5ba1b72ca99ad30c1
DevBaddy <warbaddy19+github@gmail.com>
2024-02-11 16:52:17 -0400

    Added Week 046 description

--- week-046.py
+++ week-046.py
@@ -1,3 +1,11 @@
+# https://leetcode.com/problems/most-common-word/
+
+# Given a string paragraph and a string array of the banned words banned, return the most
+# frequent word that is not banned. It is guaranteed there is at least one word that is not banned,
+# and that the answer is unique.
+
+# The words in paragraph are case-insensitive and the answer should be returned in lowercase.
+
 def mostCommonWord(paragraph: str, banned: list[str]) -> str:
     paragraph = paragraph.lower()
     wordCount = {}
