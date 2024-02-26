cf3cad1d68029cdfdc241e95b1a4a236614b134a
DevBaddy <warbaddy19+github@gmail.com>
2024-02-26 08:09:12 -0400

    Added Week 050 link and description

--- week-050.py
+++ week-050.py
@@ -1,3 +1,9 @@
+# https://leetcode.com/problems/power-of-three/
+
+# Given an integer n, return true if it is a power of three. Otherwise, return false.
+
+# An integer n is a power of three, if there exists an integer x such that n == 3x.
+
 def isPowerOfThree(n: int) -> bool:
     if n < 1:
         return False
