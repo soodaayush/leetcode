f348d5bb2075cc95909fb7d0be88218ff0dd5145
soodaayush <aayush.sood@icloud.com>
2024-03-12 12:52:07 -0300

    Added Week-051 solution, first time ever done in C++

---
+++ week-051.cpp
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/fizz-buzz/
+
+// Given an integer n, return a string array answer (1-indexed) where:
+// answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+// answer[i] == "Fizz" if i is divisible by 3.
+// answer[i] == "Buzz" if i is divisible by 5.
+// answer[i] == i (as a string) if none of the above conditions are true.
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<string> fizzBuzz(int n) {
+    vector<string> array;
+
+    for (int i = 1; i <= n; i++) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            array.push_back("FizzBuzz");
+        } else if (i % 3 == 0) {
+            array.push_back("Fizz");
+        } else if (i % 5 == 0) {
+            array.push_back("Buzz");
+        } else {
+            array.push_back(to_string(i));
+        }
+    }
+
+    return array;
+}
