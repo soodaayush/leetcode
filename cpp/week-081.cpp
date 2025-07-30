b07c3186b2ba2716e766bcb307bd351a6c056703
soodaayush <aayush.sood@icloud.com>
2025-07-30 11:19:29 -0300

    Added Week 081 solution

--- cpp/week-081.cpp
+++ cpp/week-081.cpp
@@ -1,3 +1,40 @@
-//
-// Created by aayus on 7/30/2025.
-//
+// https://leetcode.com/problems/monotonic-array/
+
+// An array is monotonic if it is either monotone increasing or monotone decreasing.
+
+// An array nums is monotone increasing if for all i <= j, nums[i] <= nums[j]. An array
+// nums is monotone decreasing if for all i <= j, nums[i] >= nums[j].
+
+// Given an integer array nums, return true if the given array is monotonic, or false
+// otherwise.
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+bool isMonotonic(vector<int>& nums) {
+    bool isIncreasing = true;
+    bool isDecreasing = true;
+
+    for (int i = 1; i < size(nums); i++) {
+        if (!isIncreasing && !isDecreasing) {
+            break;
+        }
+
+        if (nums[i] < nums[i - 1]) {
+            isIncreasing = false;
+        }
+
+        if (nums[i] > nums[i - 1]) {
+            isDecreasing = false;
+        }
+    }
+
+    return isIncreasing || isDecreasing;
+}
+
+int main() {
+    vector<int> nums {1, 2, 2, 3};
+    isMonotonic(nums);
+}
