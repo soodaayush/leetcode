6c8f04bbc9f1324743edff9cffb9124e11869e8c
soodaayush <aayush.sood@icloud.com>
2026-05-20 22:58:20 -0300

    Added Week 094 solution

---
+++ python/week-094.py
@@ -0,0 +1,55 @@
+# https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+
+# Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the
+# linked list sorted as well.
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def middleNode(self, head):
+        list_for_counting = head
+        list_for_locating = head
+
+        i = 0
+        j = 0
+
+        while list_for_counting:
+            i += 1
+            list_for_counting = list_for_counting.next
+
+        while list_for_locating:
+            if j == (i // 2):
+                return list_for_locating
+
+            list_for_locating = list_for_locating.next
+            j += 1
+
+
+
+
+
+
+a = ListNode(1)
+b = ListNode(2)
+c = ListNode(3)
+d = ListNode(4)
+e = ListNode(5)
+f = ListNode(6)
+
+a.next = b
+b.next = c
+c.next = d
+d.next = e
+e.next = f
+
+list1 = a
+
+sol = Solution()
+merged_list = sol.middleNode(list1)
+
+while merged_list:
+    print(merged_list.val, end="->")
+    merged_list = merged_list.next
