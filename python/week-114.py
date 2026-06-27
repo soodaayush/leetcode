cd73625213b8f83d29e3e17916b04c9f343835ec
soodaayush <aayush.sood@icloud.com>
2026-06-27 20:07:40 -0300

    Added Week 114 solution

---
+++ python/week-114.py
@@ -0,0 +1,24 @@
+# https://leetcode.com/problems/binary-tree-inorder-traversal/
+
+# Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def inorderTraversal(self, root: list[TreeNode]) -> list[int]:
+        result = []
+
+        def dfs(root):
+            if root is None:
+                return
+
+            dfs(root.left)
+            result.append(root.val)
+            dfs(root.right)
+
+        dfs(root)
+        return result
