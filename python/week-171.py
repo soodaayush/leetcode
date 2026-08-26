926ba8873b02c4c611cb15fb3a16905efadb77fc
Aayush Sood <aayush.sood@icloud.com>
2026-08-26 11:27:27 -0300

    Added Week 171 Solution description

--- python/week-171.py
+++ python/week-171.py
@@ -1,3 +1,5 @@
+# https://leetcode.com/problems/boats-to-save-people/
+
 # You are given an array people where people[i] is the weight of the ith person,
 # and an infinite number of boats where each boat can carry a maximum weight of
 # limit. Each boat carries at most two people at the same time, provided the sum
